6949d0a82fefb67eb6bf67978b7bfbbf8ddca600
hm-jantscha <dominik.jantschar0@hm.edu>
2019-12-15 12:56:21 +0100

    Admin Screen added. Not tested yet

--- BuddyHelper/src/app/admin-screen/admin-screen.component.ts
+++ BuddyHelper/src/app/admin-screen/admin-screen.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../example/user.example';
+import { USER } from '../data/user.data';
 @Component({
   selector: 'app-admin-screen',
   templateUrl: './admin-screen.component.html',
   styleUrls: ['./admin-screen.component.scss']
 })
-export class AdminScreenComponent implements OnInit {
+export class AdminScreenComponent {
 
-  constructor() { }
+  protected newUsername : string;
+  protected newPassword : string;
+  //Oder z.b Krankenkarte nummer etc. Zur indentifizierung
+  //wer diese Person ist.
+  private newPersonalCardNumber : string;
 
-  ngOnInit() {
+  constructor() {
   }
-  register(){
-    
+
+  register(): void{
+    let user: User = {
+      username: this.newUsername,
+      password: this.newPassword
+    }
+    if(user.username == USER.username){
+      alert("Dieser Benutzername ist bereits in verwendung")
+    }
+    else{
+      alert("Dieser Benutzername ist verfügbar")
+    }
   }
 }
